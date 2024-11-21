8496e0bce2aa828a9312f16ab2b97698c48c7325
Mohammed-HG <mohammedhg16.5@outlook.com>
2024-11-21 05:17:06 +0300

    New Ui to Index

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,31 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap'
 
 const Index = () => {
   const navigate = useNavigate();
 
-  const handleButtonClick = () => {
+  const handleLogin = () => {
     navigate('/login');
   };
 
-  const handleButtonClick1 = () => {
+  const handleRegister = () => {
     navigate('/register');
   };
 
   return (
-    <div>
-      <h1>Welcome to the Phone Book App</h1>
-      <p>Store and manage your contacts with ease!</p>
-
-      <h2>Login</h2>
-      <button onClick={handleButtonClick}>Go to Login</button>
-
-      <h2>Register</h2>
-      <button onClick={handleButtonClick1}>Go to Register</button>
-    
-    </div>
-
-    
+    <div class="container p-5 my-5 bg-dark text-white" >
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <Card className="text-center mt-4">
+              <Card.Body>
+                <Card.Title>Welcome to the Phone Book App</Card.Title>
+                <Card.Text>Store and manage your contacts with ease!</Card.Text>
+                <Button variant="primary" onClick={handleLogin} className="m-2"> Login </Button>
+                <div className="d-grid gap-2"></div>
+                <h6>-OR-</h6>
+                <div className="d-grid gap-2"></div>
+                <Button variant="secondary" onClick={handleRegister} className="m-2"> Register </Button>
+             </Card.Body>
+           </Card>
+         </Col>
+       </Row>
+     </Container>
+  </div>
   );
 };
 
