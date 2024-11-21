285601ae00d2be642dc91aa1972fb4199f521380
Mohammed-HG <mohammedhg16.5@outlook.com>
2024-11-21 05:17:54 +0300

    Add Some pages and route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
 import Index from './components/Index';
 import Register from './components/Register';
 import Login from './components/Login';
 import Main from './components/Main';
+import AddContact from './components/AddContact';
+import NavBar from './adds/NavBar';
+import MessageModal from './components/MessageModal';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@react-login-page/page5';
 
 function App() {
   return (
@@ -12,9 +18,11 @@ function App() {
       <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/main" element={<Main />} />
           <Route path="/register" element={<Register />} />
-
+          <Route path="/Home" element={<Main />} />
+          <Route path="/Add-Contact" element={<AddContact />} />
+          <Route path="/NavBar" element={<NavBar />} />
+          <Route path="/MessageModal" element={<MessageModal />} />
 
         </Routes>
       </div>
