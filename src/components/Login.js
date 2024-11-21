8e213d6886fd1f98a658d62eb5990238329bd655
Mohammed-HG <mohammedhg16.5@outlook.com>
2024-11-21 05:13:53 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,41 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import MessageModal from './MessageModal';
+import { Container } from 'react-bootstrap';
 
 const Login = () => {
+
+    const [modalShow, setModalShow] = useState(false); 
+    const [modalTitle, setModalTitle] = useState(''); 
+    const [modalMessage, setModalMessage] = useState(''); 
+
+    const handleClose = () => setModalShow(false);
+    
     const [username, setusername] = useState('');
     const [password, setpassword] = useState('');
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
+    useEffect(() => {
+      if (modalShow) {
+        const timer = setTimeout(() => { 
+          handleClose();
+          navigate('/Home');
+        }, 1500);
+        return () => clearTimeout(timer);
+      }
+    },[modalShow, navigate]);
+
+    const handleLogin = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://127.0.0.1:3000/api/login', {username, password});
-            alert(response.data.message || 'Login Successful');
+            setModalTitle('Login Successful');
+            setModalMessage(response.data.message || 'Login Successful');
+            setModalShow(true);
+            
             if (response.data.token) {
                 localStorage.setItem('token', response.data.token);
-                navigate('/main')
             }
         } catch(error) {
-            alert(error.response?.data || 'Login Failed');
+          setModalTitle('Login Error'); 
+          setModalMessage('Username OR Password Invaild');
+          setModalShow(true);
         }
     }
 
-return (
-    <form onSubmit={handleSubmit}>
+    return (
+      <form onSubmit={handleLogin} class="was-validated">
+        <Container >
         <h2>Login</h2>
-        <div>
-            <label>User Name</label>
-            <input type='text' value={username} onChange={(e) => setusername(e.target.value)} required />
+        <div class="mb-3 mt-3">
+
+          <input type="text" 
+           value={username} 
+           onChange={(e) => setusername(e.target.value)} 
+           class="form-control"
+           id="floatingInput" 
+           placeholder="Enter username" 
+           name="uname" 
+           required 
+           />
+
+          <div class="valid-feedback">Valid.</div>
+          <div class="invalid-feedback">Please fill out this field.</div>
         </div>
-        <div>
-            <label>Password</label>
-            <input type="password" value={password} onChange={(e) => setpassword(e.target.value)} required />
-        </div>
-        <button type="submit">Login</button>
+      <div class="mb-3">
+
+         <input 
+         type="password" 
+         value={password}
+         onChange={(e) => setpassword(e.target.value)}
+         class="form-control"
+         id="floatingPassword" 
+         placeholder="Enter password" 
+         name="pswd" 
+         required 
+         />
+
+         <div class="valid-feedback">Valid.</div>
+         <div class="invalid-feedback">Please fill out this field.</div>
+      </div>
+      <div class="form-check mb-3">
+        <input class="form-check-input" type="checkbox" id="myCheck" name="remember" required />
+        <label class="form-check-label" for="myCheck">I agree on blabla.</label>
+        <div class="valid-feedback">Valid.</div>
+        <div class="invalid-feedback">Check this checkbox to continue.</div>
+     </div>
+
+     <div> 
+      <button onClick={handleLogin} 
+       type="submit" 
+       class="btn btn-primary">Login</button> 
+      <MessageModal 
+      show={modalShow} 
+      handleClose={handleClose} 
+      title={modalTitle} 
+      message={modalMessage} 
+      /> 
+      </div>
+    </Container>
     </form>
-    
-  );
+    );
+
 };
 
 export default Login;
